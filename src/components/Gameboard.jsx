5909152be58c996fb704fb371cebf4e1d14c904d
Alonso8729 <alon8729@gmail.com>
2023-11-27 13:21:38 +0200

    Changed props

--- src/components/Gameboard.jsx
+++ src/components/Gameboard.jsx
@@ -2,16 +2,13 @@ import '../styles/Gameboard.css'
 import Card from '../components/Card'
 
 export default function Gameboard({ cards, handleClick }) {
-
+    console.log(cards)
     return (
         <div className="gameboard">
             {cards && cards.map(card =>
                 <Card
-                    key={card.id}
                     handleClick={handleClick}
-                    id={card.id}
-                    image={card.image}
-                    name={card.name}
+                    card={card}
                 />)}
         </div>
     )
