68b69a26dd3d47e3a484e7a442bd7785e56d30d7
Alonso8729 <alon8729@gmail.com>
2023-11-28 14:23:34 +0200

    Added buttons

--- src/components/StartGame.jsx
+++ src/components/StartGame.jsx
@@ -2,10 +2,15 @@ import '../styles/StartGame.css'
 import bgImage from '../assets/hp-logo.png'
 
 
-export default function StartGame() {
+export default function StartGame({ handleStartGame, handleInfo }) {
     return (
         <div className='start-container'>
-            <img src={`${bgImage}`} className="image-card"/>
+            <img src={`${bgImage}`} className="image-card" />
+            <h2 className='start-title'>Memory Game</h2>
+            <div className="buttons">
+                <button onClick={handleStartGame} id='start-btn'>Start Game</button>
+                <button onClick={handleInfo} id='info-btn'>How to Play</button>
+            </div>
         </div>
     )
 }
